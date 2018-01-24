d721f50a7ca5c007d954be7bddab7b77db166d87
hytd <my@hytd.org>
2018-01-24 15:15:21 +0800

    adb_wechat.sh

---
+++ sh/adb/sh/adb_wechat.sh
@@ -0,0 +1,29 @@
+id=
+
+cp_voice2() {
+	adb pull /sdcard/tencent/MicroMsg/${id}/voice2
+}
+
+cp_weixin() {
+	adb pull /sdcard/tencent/MicroMsg/WeiXin ./weixin
+}
+
+mv_voice2() {
+	if test ! -d ./voice
+	then
+		mkdir ./voice
+	fi
+
+	for i in `find ./voice2 -name "*.amr"`
+	do
+		mv -v $i ./voice
+	done
+}
+
+rm_wechat_voice2() {
+	adb shell rm -f /sdcard/tencent/MicroMsg/${id}/voice2/*
+}
+
+rm_wechat_weixin() {
+	adb shell rm -f /sdcard/tencent/MicroMsg/WeiXin/*
+}
